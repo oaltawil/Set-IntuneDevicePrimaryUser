2a609e9f6317774b31622b57e55bf52eb72e1a07
Osama Altawil <no-reply>
2024-01-03 23:08:02 -0500

    Better output messages.

--- Set-IntuneDevicePrimaryUser.ps1
+++ Set-IntuneDevicePrimaryUser.ps1
@@ -11,14 +11,13 @@ The Set-IntuneDevicePrimaryUser.ps1 script configures the primary user of an Int
 2. The script retrieves all the Sign In Audit logs for the "Windows Sign In" application for the last 30 days
 3. For each device, the script determines the user with the highest number of sign-ins to Windows.
 4. The script compares and updates the Intune device's primary user to the most frequently signed-in user.
-5. The script creates a Csv file in the same folder as the script titled "IntuneDevices-PrimaryUsers-Date" with the following details: "IntuneDeviceId,DisplayName,CurrentPrimaryUser,NewPrimaryUser,Modified"
+5. The script creates a Csv file in the same folder as the script titled "IntuneDevices-PrimaryUsers-DateTime.csv" with the following details:
+    Format of output log file: IntuneDevices-PrimaryUsers-DateTime.csv":
+    "IntuneDeviceId,DisplayName,CurrentPrimaryUser,NewPrimaryUser,Modified"
 
 Notes:
-- If the most-frequently signed-in user could not be determined, e.g. there are no sign-in events to Windows, the script will return "Failed" for the NewPrimaryUser field in the output file and won't make any changes.
-- If the device does not have a primary user, the script will use "None" for the PreviousPrimaryUser field
-- The script will skip a device if any of the following conditions happen:
-    - The device is not managed by Intune
-    - The most-frequently signed-in user does not exist in Azure AD
+- If the most-frequently signed-in user could not be determined, e.g. there are no sign-in events to Windows, or the user no longer exists in Azure AD, the script will return "Failed" for the NewPrimaryUser field in the output file and won't make any changes.
+- If the device does not have a primary user, the script will use "None" for the CurrentPrimaryUser field
 
 .PARAMETER GroupName
 The name of a Group that contains Intune devices
@@ -50,7 +49,7 @@ param (
 $ErrorActionPreference = 'Stop'
 
 # Connect to Microsoft Graph and request the ability to read Audit logs and Users and modify Intune Devices
-Connect-MgGraph -Scopes AuditLog.Read.All, User.Read.All, DeviceManagementManagedDevices.ReadWrite.All, Group.Read.All, GroupMember.Read.All, Device.Read.All
+Connect-MgGraph -Scopes AuditLog.Read.All, User.Read.All, DeviceManagementManagedDevices.ReadWrite.All, Group.Read.All, GroupMember.Read.All, Device.Read.All -NoWelcome
 
 #
 #
@@ -80,10 +79,12 @@ Set-Content -Path $OutputFilePath -Value "IntuneDeviceId,DisplayName,CurrentPrim
 if ($InputFilePath) {
 
     # Verify that the Csv input file path is valid
-    Get-Item -Path $InputFilePath
+    Get-Item -Path $InputFilePath | Out-Null
 
     # Import the contents of the Csv file into a collection of PSCustomObject objects
     $AllDevices = Import-Csv -Path $InputFilePath
+
+    Write-Host "`nImported the CSV Input File $InputFilePath"
 
     # Retrieve the note properties (static properties) of the PSCustomObject objects
     $ColumnHeaders = $AllDevices | Get-Member -MemberType NoteProperty | Sort-Object -Property Name -Descending
@@ -91,7 +92,7 @@ if ($InputFilePath) {
     # Verify that the PSCustomObject objects have 2 properties: DeviceId and DisplayName
     if (($ColumnHeaders[0].Name -ne "IntuneDeviceId") -or ($ColumnHeaders[1].Name -ne "DisplayName")) {
 
-        Write-Error "The script requires a Csv file that has 2 column headers: IntuneDeviceId and DisplayName."
+        Write-Error "`nThe script requires a CSV file that has 2 column headers: IntuneDeviceId and DisplayName."
 
     }
 
@@ -111,7 +112,7 @@ if ($GroupName) {
 
     if (-not $Group){
 
-        Write-Error "Unable to find an Azure AD group named '$GroupName'."
+        Write-Error "`nUnable to find an Azure AD group named '$GroupName'."
     }
 
     # Retrieve all Device members of the Group
@@ -119,7 +120,7 @@ if ($GroupName) {
 
     if (-not $DirectoryObjects) {
 
-        Write-Error "Failed to find any devices that are members of group '$GroupName'."
+        Write-Error "`nFailed to find any devices that are members of group '$GroupName'."
     }
 
     # Initialize the $AllDevices variable to an empty array
@@ -139,7 +140,7 @@ if ($GroupName) {
         # If the device is not managed by Intune, print a warning and skip to the next device
         if (-not $ManagedDevice) {
     
-            Write-Warning "Failed to retrieve the Intune managed device with Display Name $DisplayName and Azure AD Device ID $AzureAdDeviceId."
+            Write-Warning "`nFailed to retrieve the Intune managed device with Display Name $DisplayName and Azure AD Device ID $AzureAdDeviceId."
     
             # Skip this particular device and move on to the next one
             Continue
@@ -158,6 +159,8 @@ if ($GroupName) {
     }
 
 }
+
+Write-Host "`nRetrieving the last 30 days of Azure AD Audit Sign-In Logs for the 'Windows Sign In' application.`nThis command will take several minutes to complete..."
 
 # Retrieve all the sign-in events to Windows
 $AllSignInLogs = Get-MgAuditLogSignIn -All -Filter "appDisplayName eq 'Windows Sign In'"
@@ -184,7 +187,7 @@ foreach ($Device in $AllDevices) {
     # If the device is not managed by Intune, print a warning and skip to the next device
     if (-not $ManagedDevice) {
 
-        Write-Warning "Failed to retrieve the Intune managed device with Display Name $DisplayName and Intune Device ID $IntuneDeviceId."
+        Write-Warning "`nFailed to retrieve the Intune managed device with Display Name $DisplayName and Intune Device ID $IntuneDeviceId."
 
         # Skip this particular device and move on to the next one
         Continue
@@ -215,7 +218,7 @@ foreach ($Device in $AllDevices) {
         # Verify that the most frequent user has a valid Azure AD User object
         if (-not $MostFrequentUser) {
     
-            $ErrorMessage = "The most frequent user '$MostFrequentUserUpn' for $DisplayName does not exist."
+            $ErrorMessage = "The most frequent user $MostFrequentUserUpn for $DisplayName does not exist."
 
             Write-Warning $ErrorMessage
 
@@ -228,7 +231,7 @@ foreach ($Device in $AllDevices) {
     # Failed to determine the user who signed-in the most to the device
     else {
 
-        $ErrorMessage = "Failed to discover the most frequent user for $DisplayName (the device had no 'Windows Sign In' entries)."
+        $ErrorMessage = "Failed to discover the most frequent user for $DisplayName (the device had no 'Windows Sign In' entries)"
 
         Write-Warning $ErrorMessage
 
@@ -267,7 +270,7 @@ foreach ($Device in $AllDevices) {
     {
         $Modified = "No"
 
-        $ErrorMessage = "The most frequently-signed user is already configured as the primary user"
+        $ErrorMessage = "No change required: The current primary user $PrimaryUserUpn is the most frequently-signed user to $DisplayName"
     }
     # The most frequent user and the primary user are different and the most frequent user has been determined
     elseif ($MostFrequentUserUpn -ne "Failed") 
@@ -282,7 +285,7 @@ foreach ($Device in $AllDevices) {
 
         try {
 
-            Invoke-MgGraphRequest -Method $Method -Uri $Uri -Body $Body -ErrorAction Continue
+            Invoke-MgGraphRequest -Method $Method -Uri $Uri -Body $Body
 
         }
         catch {
@@ -302,8 +305,10 @@ foreach ($Device in $AllDevices) {
             }
             else {
 
-                $Modified = "Yes"
+                Write-Host "Successfully configured the primary user for $DisplayName to $MostFrequentUserUpn"
 
+                $Modified = "Yes"
+                
             }
 
         }
